dd1205e83095ac7530dd032dd42d0917a4dee4fa
Felipe Sucupira <58998427+felipeSucupiraO@users.noreply.github.com>
2025-01-07 14:24:14 +0000

    feat: Initial main components added

---
+++ task-manager/src/components/TaskAdder.jsx
@@ -0,0 +1,12 @@
+function TaskAdder() {
+    return (
+        <div>
+            <h2>Task Adder</h2>
+            <input type="text" placeholder="Task Title"/>
+            <input type="text" placeholder="Task Description"/>
+            <button>Add</button>
+        </div>
+    )
+}
+
+export default TaskAdder;
