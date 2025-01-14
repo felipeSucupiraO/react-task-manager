ef75bec6dacc64c82f4097397623abeb8d7a6ce4
Felipe Sucupira <58998427+felipeSucupiraO@users.noreply.github.com>
2025-01-14 14:36:45 +0000

    feat: added the task details page

--- task-manager/src/components/TaskUnit.jsx
+++ task-manager/src/components/TaskUnit.jsx
@@ -1,19 +1,33 @@
 import { Trash2 } from "lucide-react"
 import { MoveRight } from "lucide-react"
+import { useNavigate } from "react-router"
 
 function TaskUnit(props) {
+    const task = props.task;
+
+    const navigate = useNavigate();
+
+    function navigateToTaskDescription() {
+        navigate(`/task-details/${task.title}/${task.description}`)
+    }
+
     return (
-        <li key={props.task.id} className="flex flex-row gap-2">
+        <li key={task.id} className="flex flex-row gap-2">
             <button
-                onClick={() => {props.changeTaskCompletionOnId(props.task.id)}}
-                className={`flex-1 bg-dark-gray p-4 text-white text-left rounded-lg ${props.task.isCompleted ? "line-through" : ""}`}
+                onClick={() => {props.changeTaskCompletionOnId(task.id)}}
+                className={`flex-1 bg-dark-gray p-4 text-white text-left rounded-lg ${task.isCompleted ? "line-through" : ""}`}
             >
-                {props.task.title}
+                {task.title}
             </button>
-            <button className="flex-none bg-dark-gray p-3 text-white rounded-lg"><MoveRight size={20} /></button>
+            <button 
+                onClick={() => {navigateToTaskDescription()}}
+                className="flex-none bg-dark-gray p-3 text-white rounded-lg"
+            >
+                <MoveRight size={20} />
+            </button>
             <button 
                 className="flex-none bg-dark-gray p-3 text-white rounded-lg"
-                onClick={() => {props.deleteTaskOnId(props.task.id)}}
+                onClick={() => {props.deleteTaskOnId(task.id)}}
             ><Trash2 size={20} /></button>
         </li>
     )
